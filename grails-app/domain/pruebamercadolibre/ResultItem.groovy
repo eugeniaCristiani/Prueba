a62efe099e06a5bf5ce1820fc7929396fa1d807e
Maykol Irigoyen <maykol.irigoyen@globant.com>
2012-04-20 11:22:35 -0300

    Precio agregado

--- grails-app/domain/pruebamercadolibre/ResultItem.groovy
+++ grails-app/domain/pruebamercadolibre/ResultItem.groovy
@@ -4,10 +4,12 @@ class ResultItem {
 	String thumbnailURL
 	String permalink
 	String title
+	String precio
 	
     static constraints = {
 		title(blank: false)
 		permalink(blank: false)
 		thumbnailURL(blank:false)
+		precio(blank:false)
     }
 }
